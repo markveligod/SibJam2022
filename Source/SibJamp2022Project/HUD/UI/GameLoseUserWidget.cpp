314cbb8f5e98c5c5f7da3c413ddb0c7008bbc750
Mark <34418187+markveligod@users.noreply.github.com>
2022-05-02 17:09:11 +0300

    add font and replace image press E

--- Source/SibJamp2022Project/HUD/UI/GameLoseUserWidget.cpp
+++ Source/SibJamp2022Project/HUD/UI/GameLoseUserWidget.cpp
@@ -18,9 +18,8 @@ void UGameLoseUserWidget::OnStart()
     if (!CHECK(GameInst != nullptr, FString("Game inst is nullptr"))) return;
 
     ACharacter* Character = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
-
+    APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
     Character->GetCharacterMovement()->DisableMovement();
-    Character->GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
     Character->GetMesh()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
     Character->GetMesh()->SetSimulatePhysics(true);
     
